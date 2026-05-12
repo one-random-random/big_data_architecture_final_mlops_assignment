254103a17b9dd14525e893bb1a12274eb2ae6728
unknown <L00196770@atu.ie>
2026-05-12 22:13:00 +0100

    Adding training of the model based on the newly cleaned data
    - Added new train.py which loads up the cleaned data, creates a LinearRegression model, trains it on 80% of the data and dumps the resulting model into a .pkl file that I can use in next phase.
    - Added constants file to keep all the file names and other constants in one place to make things easier as I go.
    - Updated the clean_data.yaml workflow to now also train the model on the cleaned data. Might rename in the future to better represent what it does.

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+SOURCE_DATA_FILE_NAME = "Salary_Data.csv"
+CLEANED_DATA_FILE_NAME = "cleaned_salary_data.csv"
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
---
+++ src/train.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from pandas import read_csv
+from joblib import dump
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+from constants import CLEANED_DATA_FILE_NAME, TEST_SIZE, RANDOM_STATE
+
+project_root = Path(__file__).resolve().parent.parent
+project_src_dir = project_root / 'src'
+data_path = project_root / 'data'
+cleaned_data_path = data_path / CLEANED_DATA_FILE_NAME
+
+if os.path.exists(cleaned_data_path):
+    print(f"File found: {cleaned_data_path}")
+else:
+    print(f"File NOT found at! Run the clean_data script! File expected: {cleaned_data_path} ")
+
+df = read_csv(cleaned_data_path)
+print(df.head())
+
+X = df["YearsExperience"].values.reshape(10, 1)
+y = df["Salary"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+dump(model, "YearsExperienceSalaryModel.pkl")
