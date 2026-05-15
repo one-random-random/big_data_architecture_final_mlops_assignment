3188d9086c9ac5b52f2e976c443f9d7213bc6d14
unknown <L00196770@atu.ie>
2026-05-15 16:43:58 +0100

    Adding MLFlow to the app and test run locally
    - Added MLFlow to the train.py file so that I can log and track the model runs and results. Want to use this later as a way to rollback if a version is poor or even as a sort of gate check before deploying a new model.
    - Moved requirements.txt to root as should ahve been.
    - Added steps for running MLFlow on local to Readme as quicker the uploading to VM to run there like in the tutorials.
    - Adding initial pickle file from the training of the model locally, might remove this later but having it initially.

--- src/train.py
+++ src/train.py
@@ -2,9 +2,12 @@ import os
 from pathlib import Path
 
 from pandas import read_csv
+import mlflow
 from joblib import dump
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error
 
 from constants import CLEANED_DATA_FILE_NAME, TEST_SIZE, RANDOM_STATE
 
@@ -27,6 +30,26 @@ y = df["Salary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+## Do the mlflow run
+with mlflow.start_run():
+    # initialise and train
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    r2 = r2_score(y_test, predictions)
+    mse = mean_squared_error(y_test, predictions)
+
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("mse", mse)
+
+    result = mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
+
+    mlflow.register_model(
+        model_uri=result.model_uri,
+        name="my-linear-salary-registered-model"
+    )
+
+    print(f"Modle r2 score: {r2}")
+
 dump(model, f"{models_path}/YearsExperienceSalaryModel.pkl")
