3ecf43c7059d7f91ef2e913ceb9586d6a542e7cf
unknown <L00196770@atu.ie>
2026-05-16 19:26:19 +0100

    Adding continiou training workflow and conditional deploy of new model
    - Added continious_training workflow which runs on specific file changes, it has specific steps to check metrics of the model after training. This check will either let the workflow continue if model is greater then X r2 score, or will throw a failure if below it. On success it will upload the artifcates to github.
    - Updated both docker_publish to trigger based off the continious_training completing and adding a check of the conclusion was successful. This builds and uploads the image.
    - Updated docker_deploy to trigger based off the docker_publish completing and adding a check of the conclusion was successful. This pulls and runs the image on self-host Google Cloud VM runner in detached mode, same as before. Also added a smoke test after starting the app to verify the app is alive. Should update the app.py to have a health api check which I could use here.
    - Added production_metrics.json which has a minimum r2 score value for a model to be better then before being used.
    - Added check_metrics.py which just does a comparison of the newly run model results against the production_metrics values, if less then throws an error which when used in a workflow will cause it to stop, if greater then will print and let workflow continue where it is used.
    - Updated train.py to create a new file candidate_metrics and write to it the scores and details of the model just run.

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
 SOURCE_DATA_FILE_NAME = "Salary_Data.csv"
 CLEANED_DATA_FILE_NAME = "cleaned_salary_data.csv"
 MODEL_FILE_NAME = "YearsExperienceSalaryModel.pkl"
+PRODUCTION_METRICS_FILE_NAME = "production_metrics.json"
+CANDIDATE_METRICS_FILE_NAME = "candidate_metrics.json"
 TEST_SIZE = 0.2
 RANDOM_STATE = 42
